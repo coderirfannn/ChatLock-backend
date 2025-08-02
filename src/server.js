de94642b577ddb424e4f7392a1bfbbc169b29ec8
Coder Irfan <mohdirfan70097@gmail.com>
2025-08-03 00:59:57 +0530

    Complete ChatLock-v1-backend

--- src/server.js
+++ src/server.js
@@ -34,8 +34,9 @@ app.use((err,req,res ,next)=>{
 const startServer = async () => {
     try {
         await connetDB();
+        if(ENV.NODE_ENV !== "production"){
         app.listen(ENV.PORT, () => console.log(`Server is running.. on ${ENV.PORT}`))
-
+        }
     } catch (error) {
         console.error("Failed to start server", error.message);
         process.exit(1);
