4c4e9597854d2ae8a18837a909d50cca79b9c4ec
Coder Irfan <mohdirfan70097@gmail.com>
2025-08-03 01:20:45 +0530

    Versal

--- src/config/env.js
+++ src/config/env.js
@@ -6,7 +6,7 @@ export const ENV={
     PORT:process.env.PORT,
     NODE_ENV:process.env.NODE_ENV,
     MONGO_URI:process.env.MONGO_URI,
-    CLERK_PUBLICATION_KEY:process.env.CLERK_PUBLICATION_KEY,
+    CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
     ARCJET_ENV:process.env.ARCJET_ENV,
     ARCJET_KEY:process.env.ARCJET_KEY,
